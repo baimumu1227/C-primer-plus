d4413e15fae034d3ccae823b397c7df45e25960f
baimumu1227 <2362502384@qq.com>
2021-10-16 20:11:47 +0800

    init

---
+++ chapter 1/myfirst.cpp
@@ -0,0 +1,11 @@
+// my first.cpp --display a message
+
+#include<iostream>                                  // a PREPROCESSOR directive 
+int main()                                          // function header 
+{                                                   // start of function body
+    using namespace std;                            // make definitions visible
+    cout << "Come up and C++ me some time.";        // message
+    cout << endl;                                   // start a new line
+    cout << "You won't regret it!" << endl;         // more output
+    return 0;                                       // teminate main()
+}                                                   // end of function body
---
+++ chapter 1/using.cpp
@@ -0,0 +1,9 @@
+// this file is to practice the using directive
+
+#include<iostream>
+int main()
+{
+    // using std::cout;
+    std::cout << "this is a test of using directive";
+    return 0;
+}
