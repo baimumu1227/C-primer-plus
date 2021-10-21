b2e21ccb30378b7ba0b68731779e8631b25974c8
baimumu1227 <2362502384@qq.com>
2021-10-21 16:37:06 +0800

    init

---
+++ chapter3/exceed.cpp
@@ -0,0 +1,29 @@
+// exceed.cpp -- exceeding some integer limits
+#include<iostream>
+#include<climits>
+#define ZERO 0
+int main()
+{
+    using namespace std;
+    short sam = SHRT_MAX;
+    unsigned short sue = sam;
+
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited." << endl
+         << "Add $1 to each acount." << endl << "Now ";
+    
+    sam = sam + 1;
+    sue = sue + 1;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited.\nPoor Sam!" << endl;
+    sam = ZERO;
+    sue = ZERO;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited." << endl
+         << "Take $1 to each acount." << endl << "Now ";
+    sam = sam - 1;
+    sue = sue - 1;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited.\nLuck Sue!" << endl;
+    return 0;
+}
